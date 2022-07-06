092c3d1a03eb1b1cbe7bf0b79f8b752bd807ec84
Vladimir444k <gluschenkovladimir337@gmail.com>
2022-07-06 22:16:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // входные данные
+        int amount = 250;
+        int bonus = 20;
+
+        // логика программы
+        int mile = amount / bonus;
+
+        System.out.printf("Бонуных миль составит: " + mile);
+    }
+}
